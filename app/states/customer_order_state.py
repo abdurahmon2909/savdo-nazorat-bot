df72f63145bc13cda40c14cccd8dfedaf0055518
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-04-21 01:29:48 +0500

    fix: customer order state ichiga quantity_custom qo'shildi

--- app/states/customer_order_state.py
+++ app/states/customer_order_state.py
@@ -4,7 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class CreateCustomerOrderRequestState(StatesGroup):
     category = State()
     product = State()
-    quantity = State()
+    quantity_custom = State()
     add_more = State()
     payment_type = State()
     confirm = State()
