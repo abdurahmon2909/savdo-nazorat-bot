ef87ff60b6040011278118e209ad5a921108b70a
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-04-21 01:27:25 +0500

    fix: admin_orders router olib tashlandi (inline systemga o'tildi)

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -5,7 +5,6 @@ from app.handlers.admin_debtors import router as admin_debtors_router
 from app.handlers.admin_history import router as admin_history_router
 from app.handlers.admin_order_inline import router as admin_order_inline_router
 from app.handlers.admin_order_requests import router as admin_order_requests_router
-from app.handlers.admin_orders import router as admin_orders_router
 from app.handlers.admin_overdue import router as admin_overdue_router
 from app.handlers.admin_panel_inline import router as admin_panel_inline_router
 from app.handlers.admin_payments import router as admin_payments_router
@@ -22,7 +21,6 @@ def setup_routers() -> Router:
     r.include_router(start_router)
     r.include_router(admin_customers_router)
     r.include_router(admin_products_router)
-    r.include_router(admin_orders_router)
     r.include_router(admin_order_requests_router)
     r.include_router(admin_order_inline_router)
     r.include_router(admin_payments_router)
